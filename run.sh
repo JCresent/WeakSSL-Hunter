51d41b40086cee2d0671a2b805df7f9a52e79402
Justin Cresent <JCresent@hotmail.com>
2024-04-13 21:39:26 -0400

    Added in all shell commands to run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+Cresent-analyze.py -i apps/GCash.apk -o GCash_output.txt
+Cresent-analyze.py -i apps/Money On Mobile.apk -o MOM_output.txt
+Cresent-analyze.py -i apps/mPAY.apk -o mPAY_output.txt
+Cresent-analyze.py -i apps/OxigenWallet.apk -o OxigenWallet_output.txt
+Cresent-analyze.py -i apps/Zuum.apk -o Zuum_output.txt
