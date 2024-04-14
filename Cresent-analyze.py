f768b2d15e5c354ee637248c1445c0d12f9d7660
Justin Cresent <JCresent@hotmail.com>
2024-04-14 14:07:13 -0400

    findInternetPermission working

--- Cresent-analyze.py
+++ Cresent-analyze.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys 
 
 outputText = None;
@@ -13,14 +14,25 @@ def inputValidation(argc, argv):
     if argv[1] != "-i" or argv[3] != "-o":
         return False
     
-    #Get apk to analyze
+    #Get apk to analyze, create output file for results to be written
     outputText = open(argv[4], "w")
     outputText.close()
     outputText = open(argv[4], "a")
     return argv[2]
 
-def findPermissions():
-    pass
+def findInternetPermission(app):
+    #Change directory to the decompiled app directory
+    os.chdir(app)
+    permission = "android.permission.INTERNET"
+    manifest = "AndroidManifest.xml"
+    grep_command = f"grep -r '{permission}' {manifest}"
+    result = subprocess.run(grep_command, shell=True, capture_output=True, text=True)
+
+    # Print the output
+    #print(result.stdout)
+    if len(result.stdout) == 0:
+        return False
+    return True
 
 def analyzeSmali():
     pass
@@ -31,9 +43,24 @@ def main():
     if not inputAPK:
         print("Invalid input, try: python Cresent-analyze.py -i target-app.apk -o output.txt")
         sys.exit(1)
+    print("APK path: " + inputAPK)
 
     #Use apktool to decompile APK
-    os.system("apktool d " + inputAPK)
+    # os.system("apktool d " + inputAPK)
+
+    #Slice apk path to get path to the decomplied app directory 
+    start = inputAPK.find("/") + 1
+    end = inputAPK.find(".apk")
+    app = inputAPK[start:end]
+    print("App name: " + app)
+
+    #First checking if app has INTERNET permission, if not no need to check for SSL errors
+    if findInternetPermission(app) == True:
+        #print("App has INTERNET permission")
+        analyzeSmali()
+    else:
+        print("App does not have INTERNET permission, no need to go any further. Exiting...")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python Cresent-analyze.py -i "apps/GCash.apk" -o GCash_output.txt
-python Cresent-analyze.py -i "apps/Money On Mobile.apk" -o MOM_output.txt
-python Cresent-analyze.py -i "apps/mPAY.apk" -o mPAY_output.txt
-python Cresent-analyze.py -i "apps/OxigenWallet.apk" -o OxigenWallet_output.txt
-python Cresent-analyze.py -i "apps/Zuum.apk" -o Zuum_output.txt
+python Cresent-analyze.py -i apps/"GCash.apk" -o GCash_output.txt
+python Cresent-analyze.py -i apps/"Money On Mobile.apk" -o MOM_output.txt
+python Cresent-analyze.py -i apps/"mPAY.apk" -o mPAY_output.txt
+python Cresent-analyze.py -i apps/"OxigenWallet.apk" -o OxigenWallet_output.txt
+python Cresent-analyze.py -i apps/"Zuum.apk" -o Zuum_output.txt
